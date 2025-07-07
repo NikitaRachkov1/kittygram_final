614bf30e2528a96a38ecc38032d133c32c516d0c
Никита Рачков <nikita.rachkov@yandex.ru>
2025-07-07 18:08:53 +0300

    'update'

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,14 +3,20 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = (
-    'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)'
-    'abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv(
+    'SECRET_KEY',
+    'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$',
 )
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', True)
 
-ALLOWED_HOSTS = ['*']
+hosts = os.getenv('ALLOWED_HOSTS', '')
+if hosts.strip() == '*':
+    ALLOWED_HOSTS = ['*']
+else:
+    ALLOWED_HOSTS = [
+        host.strip() for host in hosts.split(',') if host.strip()
+    ]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
